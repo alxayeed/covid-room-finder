f0495d314baaa74cd1239b257b5d3c44eea16faf
alxayeed <alxayeed@gmail.com>
2021-02-17 17:48:52 +0600

    login functionality added

--- covidroom/user/urls.py
+++ covidroom/user/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
+    path('register/', views.register_user, name='register'),
+    path('login/', views.login_user, name='login'),
 ]
--- covidroom/user/views.py
+++ covidroom/user/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import RegisterForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
 
 def index(request):
     return HttpResponse('This is the index page')
 
 
-def register(request):
+def register_user(request):
     form = RegisterForm()
 
     if request.method == "POST":
@@ -23,5 +25,17 @@ def register(request):
 
     return render(request, 'user/register_form.html', {'form': form})
 
-def login(request):
-    pass
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponse(f'login successful for {user}')
+        else:
+            messages.error(request, 'Username or Password is incorrect')
+    return render(request, 'user/login.html')
