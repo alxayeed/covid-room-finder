33784ee43c4e809262e7c28f5623169bb42a3480
alxayeed <alxayeed@gmail.com>
2021-02-18 13:33:34 +0600

    Room list will now be shown dynamically on the front dropdown

--- covidroom/bookings/views.py
+++ covidroom/bookings/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Room
 
 
 @login_required(login_url='login')
 def index(request):
     print(request.user)
-    return render(request, 'user/index.html')
+    rooms = Room.objects.all()
+    print(rooms)
+    context = {
+        "rooms": rooms
+    }
+    return render(request, 'user/index.html', context)
