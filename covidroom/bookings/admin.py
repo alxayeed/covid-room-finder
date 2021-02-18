03386f827e0d376f3d3205885f8cf5e4d3b8a650
alxayeed <alxayeed@gmail.com>
2021-02-18 12:46:37 +0600

    room and booking model created

--- covidroom/bookings/admin.py
+++ covidroom/bookings/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Room, Booking
 
 # Register your models here.
+
+
+class BookingAdmin(admin.TabularInline):
+    model = Booking
+
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('name', 'capacity')
+    inlines = [BookingAdmin, ]
+
+
+admin.site.register(Room, RoomAdmin)
--- covidroom/bookings/models.py
+++ covidroom/bookings/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=100)
+    capacity = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Booking(models.Model):
+    room = models.ForeignKey(
+        Room, on_delete=models.CASCADE, related_name='booking')
+    username = models.CharField(max_length=20)
+    fullname = models.CharField(max_length=50)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.fullname
