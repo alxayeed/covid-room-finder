ddb5d43ffadf79e86e91b6acab96d7f5df7db06c
alxayeed <alxayeed@gmail.com>
2021-02-19 14:38:15 +0600

    capacity information and room full information will be displayed

--- covidroom/bookings/urls.py
+++ covidroom/bookings/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('capacity/', views.get_capacity, name='capacity'),
+    path('book/', views.book_room, name='book')
 ]
--- covidroom/bookings/views.py
+++ covidroom/bookings/views.py
@@ -1,14 +1,75 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Room
+from .models import Room, Booking
+from .forms import BookingForm
+from django.forms import ValidationError
 
 
 @login_required(login_url='login')
 def index(request):
-    print(request.user)
     rooms = Room.objects.all()
-    print(rooms)
     context = {
         "rooms": rooms
     }
     return render(request, 'user/index.html', context)
+
+
+def get_capacity(request):
+    "Get current Empty Capacity of all rooms"
+
+    date = request.POST.get('date')
+    total_capacity = 0
+    total_vacant = 0
+    for room in Room.objects.all():
+        total_capacity += room.capacity
+        total_vacant += (room.capacity - len(room.booking.filter(date=date)))
+    current_vacancy = (total_vacant / total_capacity) * 100
+
+    rooms = Room.objects.all()
+
+    context = {
+        'rooms': rooms,
+        'date': date,
+        'vacancy': f'{current_vacancy:.2f}'
+    }
+
+    return render(request, 'user/index.html', context)
+
+
+def book_room(request):
+    form = BookingForm(request.POST)
+    # print(form.errors)
+
+    room = request.POST.get('room')
+    date = request.POST.get('date')
+    name = request.POST.get('name')
+    username = request.POST.get('username')
+    # print(room, date, name, username, sep='   ')
+
+    room = Room.objects.get(name=room)
+    current_bookings = len(room.booking.filter(date=date))
+
+    if current_bookings < room.capacity:
+        print('You can book this room')
+    else:
+        bookers = []
+        for booking in room.booking.filter(date=date):
+            bookers.append(booking.fullname)
+
+        msg = f'{room} is already booked out'
+        rooms = Room.objects.all()
+        room_info = {}
+        for room in rooms:
+            empty = room.capacity - len(room.booking.filter(date=date))
+            if empty > 0:
+                room_info[room.name] = empty
+
+        context = {
+            'msg': msg,
+            'bookers': bookers,
+            "rooms": rooms,
+            "room_info": room_info
+        }
+        return render(request, 'user/index.html', context)
+
+    return redirect('/')
